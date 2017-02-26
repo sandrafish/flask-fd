27c80eae35c67e51933d69483fc0ae4f9b51c824
sandra fish <sandrafish@comcast.net>
2017-02-26 15:57:54 -0700

    freezer url

--- freeze.py
+++ freeze.py
@@ -2,6 +2,8 @@ from flask_frozen import Freezer
 from app import app, get_csv
 freezer = Freezer(app)
 
+app.config['FREEZER_BASE_URL']=True
+
 @freezer.register_generator
 def detail():
     for row in get_csv():
